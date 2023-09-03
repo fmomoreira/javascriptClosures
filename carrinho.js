15644dd7b3e5c742b086fcb61a86bc46f55cca15
Felipe Moreira <felipemoreira.frontend@gmail.com>
2023-09-03 15:39:17 -0300

    Update carrinho.js

--- carrinho.js
+++ carrinho.js
@@ -1,95 +1,79 @@
-function carrinho() {
+(function(){
+
+
+function carrinho(){
     const sacola = [];
-  
-    function encontrarItemIndex(nomeItem) {
-      return sacola.findIndex(produto => produto.name === nomeItem);
+    function addProduto(produto){
+        const index = encontrarItemIndex(produto.name)
+        if(index !== -1){
+            sacola[index].amount += produto.amount
+        } else {
+            sacola.push({
+                id: sacola.length + 1,
+                name:  produto.name,
+                description: produto.description,
+                amount: produto.amount,
+                price: produto.price
+            })
+        }
+        atualizarCarrinho();
     }
-  
-    function addProduto(produto) {
-      const index = encontrarItemIndex(produto.name);
-      if (index !== -1) {
-        sacola[index].quantidade += produto.quantidade;
-      } else {
-        sacola.push({
-          id: sacola.length + 1,
-          name: produto.name,
-          description: produto.description,
-          quantidade: produto.quantidade,
-          preco: produto.preco,
-        });
-      }
-    }
-  
-    function getSacola() {
-      return sacola;
-    }
-  
+
     function calcularTotal() {
-      let total = 0;
-      for (const produto of sacola) {
-        total += produto.preco * produto.quantidade;
-      }
-      return total;
+        let total = 0;
+        for(const produto of sacola){
+            total += produto.price * produto.amount
+        }
+        return total
     }
-  
+
+    function getSacola() {
+       return sacola;
+    }
+    function encontrarItemIndex(nomeItem){
+        return sacola.findIndex(produto => produto.name == nomeItem)
+    }
+
     return {
-      addProduto: addProduto,
-      getSacola: getSacola,
-      calcularTotal: calcularTotal,
-    };
-  }
-  
-  const minhaSacola = carrinho();
-  
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
-  
-  minhaSacola.addProduto({
-    name: "x-burger",
-    description: "Delicious burger",
-    quantidade: 2,
-    preco: 7.99,
-  });
-  
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
+        addProduto: addProduto,
+        getSacola: getSacola,
+        calcularTotal: calcularTotal
+    }
+}
 
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
+const minhaSacola = carrinho();
 
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
 
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
+const addBtns = document.querySelectorAll(".add-btn");
 
-  minhaSacola.addProduto({
-    name: "x-salada",
-    description: "Delicious salad burger",
-    quantidade: 1,
-    preco: 5.99,
-  });
-  
-  console.log(minhaSacola.getSacola()); // Imprime o conteúdo da sacola
-  console.log("Total da sacola: R$" + minhaSacola.calcularTotal().toFixed(2)); // Imprime o total da sacola
-  
+addBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const nomeItem = btn.getAttribute("data-name");
+        const precoItem = parseFloat(btn.getAttribute("data-preco"))
+        minhaSacola.addProduto({
+            name: nomeItem,
+            price: precoItem,
+            amount: 1
+        })
+    })
+})
+
+function atualizarCarrinho() {
+    const carrinhoItensDiv = document.getElementById("carrinho-itens");
+    const totalCarrinhoDiv = document.getElementById("total-carrinho");
+
+    carrinhoItensDiv.innerHTML = ""
+    totalCarrinhoDiv.textContent = ""
+
+    let total = 0;
+        minhaSacola.getSacola().forEach(item =>{
+            total += item.price * item.amount;
+            carrinhoItensDiv.innerHTML +=
+            `<div class="dropdown-item">${item.name} Qtd: ${item.amount} - R$ ${item.price.toFixed(2)}</div>`;
+        })
+
+        totalCarrinhoDiv.textContent = `Total: R$ ${total.toFixed(2)}`
+
+}
+
+})();
